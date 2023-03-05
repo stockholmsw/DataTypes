3063a214e3d5cfd28504e9c75015677962ac89c5
= <koksalocal06@gmail.com>
2023-03-05 22:26:28 +0300

    Veri Donusumu yapildi.

---
+++ TypeCasting.java
@@ -0,0 +1,22 @@
+package dataTypes;
+
+public class TypeCasting {
+    public static void main(String[] args) {
+        int number = 15;
+        double commaNumber = 22d;
+        commaNumber = number;
+        float noktaliSayi = 131.6f;
+        number = (int) noktaliSayi;
+        System.out.println(number);
+
+        byte byteSayi = 23;
+       byteSayi = (byte) noktaliSayi;
+        System.out.println(byteSayi);
+
+        int birinciDeger = 16;
+        float ikinciDeger = 11.1f;
+        double ucuncuDeger = 36;
+        birinciDeger = (int) ikinciDeger;
+        System.out.println(birinciDeger);
+    }
+}
